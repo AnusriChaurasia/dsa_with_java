e0924c2a7cb43cec2e0bd8bce3904e1f4105aae1
AnusriChaurasia <anusricc8@gmail.com>
2022-09-03 22:45:52 +0530

    Adding the code.

---
+++ src/DSAWithJava/SimpleInterest.java
@@ -0,0 +1,22 @@
+package DSAWithJava;
+
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter principle");
+        int p = sc.nextInt();
+        System.out.println("Enter rate");
+        int r = sc.nextInt();
+        System.out.println("Enter time");
+        int t = sc.nextInt();
+
+        int si = (p*r*t)/100;
+
+        System.out.println("Simple Interest = " + si);
+
+    }
+}
