25eb62b9f22a7f1e79992d30043d954a16ddbed0
AnusriChaurasia <anusricc8@gmail.com>
2022-09-03 23:03:28 +0530

    Adding the code.

--- src/DSAWithJava/PrimeNumber.java
+++ src/DSAWithJava/PrimeNumber.java
@@ -1,4 +1,25 @@
 package DSAWithJava;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
+    public static void  main(String[]args){
+
+        int div = 2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+
+        while (div<num){
+            if(num%div==0){
+                System.out.println("Not a prime number");
+                break;
+            }
+            else{
+                div = div + 1;
+            }
+        }
+
+        System.out.println("Prime Number");
+    }
 }
