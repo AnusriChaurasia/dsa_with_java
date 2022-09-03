0a2599365041deeebf30c42b636fc959d5d2ba36
AnusriChaurasia <anusricc8@gmail.com>
2022-09-03 22:55:51 +0530

    Adding the code.

--- src/DSAWithJava/MaxOfThreeNumbers.java
+++ src/DSAWithJava/MaxOfThreeNumbers.java
@@ -1,4 +1,30 @@
 package DSAWithJava;
 
+import java.util.Scanner;
+
 public class MaxOfThreeNumbers {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the three numbers ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a>b){
+            if (a>c){
+                System.out.println("Greatest number is "+a);
+            }
+        }
+        else if (b>a) {
+            if(b>c){
+                System.out.println("Greatest number is "+b);
+            }
+        }
+        else {
+            System.out.println("Greatest number is "+c);
+        }
+    }
 }
---
+++ src/DSAWithJava/PrimeNumber.java
@@ -0,0 +1,4 @@
+package DSAWithJava;
+
+public class PrimeNumber {
+}
