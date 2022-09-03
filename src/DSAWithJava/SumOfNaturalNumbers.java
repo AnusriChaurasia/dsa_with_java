4ac014ada45ea4ff97200c1f9dbca8ac9936e879
AnusriChaurasia <anusricc8@gmail.com>
2022-09-03 23:14:36 +0530

    Adding the code.

---
+++ src/DSAWithJava/SumOfNaturalNumbers.java
@@ -0,0 +1,21 @@
+package DSAWithJava;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[]args){
+
+        int sum=0;
+        int i=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number = ");
+        int num = sc.nextInt();
+
+        while (i <= num){
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("The sum of first "+num+" natural numbers is "+ sum);
+    }
+}
