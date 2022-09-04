dd2f1595bc36627be8013ffeaddb4d04641ce80c
AnusriChaurasia <anusricc8@gmail.com>
2022-09-04 20:41:18 +0530

    Added code.

---
+++ src/DSAWithJava/SimplePrograms/PrintAPattern.java
@@ -0,0 +1,13 @@
+package DSAWithJava.SimplePrograms;
+
+public class PrintAPattern {
+    public static void main(String args[]){
+
+        for (int i=4; i>0;i--){
+            for (int j=i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
